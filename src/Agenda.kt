3960ba1790a9f1d36bbdb80ab52dcc71f3e54e6e
André S Nascimento <andresoaresnascimento@gmail.com>
2024-11-01 15:12:56 -0300

    Final da Aula 11 - Agenda Com tratamentos de erro.

---
+++ src/Agenda.kt
@@ -0,0 +1,80 @@
+fun main() {
+    var choice = 0
+    val contacts = mutableListOf<Map<String, String>>()
+
+    do {
+        showMenu()
+        choice = getChoice()
+
+        when (choice) {
+            1 -> contacts.add(createContact())
+            2 -> showContacts(contacts)
+            3 -> {
+                try {
+                    contacts.removeAt(deleteContact())
+                } catch (e: IndexOutOfBoundsException) {
+                    println("Não existe contato com esse índice")
+                }
+            }
+
+            0 -> println("Até mais e obrigado pelos peixes!")
+            else -> println("Opção inválida")
+        }
+
+    } while (choice != 0)
+
+}
+
+fun showMenu() {
+    println("======MENU======")
+    println("1. Novo contato")
+    println("2. Listar Contatos")
+    println("3. Remover contato")
+    println("0. Sair")
+}
+
+fun getChoice(): Int {
+    print("> ")
+    var option = 4;
+    try {
+        option = readln().toInt()
+    } catch (e: NumberFormatException) {
+    }
+    return option
+}
+
+fun createContact(): Map<String, String> {
+    print("Nome: ")
+    val name = readln()
+    print("Email: ")
+    val email = readln()
+    print("Telefone: ")
+    val phone = readln()
+
+    println("Contato cadastrado com sucesso!")
+
+    return mapOf("name" to name, "email" to email, "phone" to phone)
+
+}
+
+fun showContacts(contacts: List<Map<String, String>>) {
+    if (contacts.isEmpty()) {
+        println("Nenhum contato cadastrado")
+    }
+    for ((index, contact) in contacts.withIndex()) {
+        val name = contact["name"]
+        val email = contact["email"]
+        val phone = contact["phone"]
+
+        println("$index - $name, $email, $phone")
+    }
+}
+
+fun deleteContact(): Int {
+    println("Qual o indice do contato que deseja remover")
+    print("> ")
+    return readln().toInt()
+
+}
+
+
